c761dcee68410c7231fa3c1707c537fda5d92b2e
fortey <fortey@inbox.ru>
2022-03-09 15:20:48 +0700

    items with bullets

--- main/item_s-bullets.go
+++ main/item_s-bullets.go
@@ -1,6 +1,6 @@
 components {
-  id: "enemy"
-  component: "/main/scripts/enemy.script"
+  id: "item"
+  component: "/main/scripts/item.script"
   position {
     x: 0.0
     y: 0.0
@@ -12,12 +12,22 @@ components {
     z: 0.0
     w: 1.0
   }
+  properties {
+    id: "name"
+    value: "shotgun"
+    type: PROPERTY_TYPE_HASH
+  }
+  properties {
+    id: "amount"
+    value: "12.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
 }
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"spider_walk\"\n"
+  "default_animation: \"s-bullets\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -37,20 +47,18 @@ embedded_components {
   id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 1.0\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"enemy\"\n"
-  "mask: \"bullets\"\n"
+  "group: \"item\"\n"
   "mask: \"player\"\n"
-  "mask: \"wall\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_SPHERE\n"
+  "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: -1.0\n"
+  "      x: -0.5\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -60,9 +68,11 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 1\n"
+  "    count: 3\n"
   "  }\n"
-  "  data: 15.0\n"
+  "  data: 7.0\n"
+  "  data: 5.7145\n"
+  "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
